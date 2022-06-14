ce7fea0adcd4b4d85f28232b4a01e83e6c1d1258
Manuel Pfeuffer <manuel.pfeuffer@esmt.org>
2022-06-14 16:31:14 +0200

    Add option for edge weights to PPM sampling

--- R/sample.R
+++ R/sample.R
@@ -3,14 +3,17 @@
 #' Sampling from the planted partition model
 #'
 #' This function samples graphs from a stochastic block model by (doing the
-#' equivalent of) Bernoulli trials for each potential edge with the probabilities
-#' given by \code{p} for in-group edges and \code{q} for between group edges.
+#' equivalent of) \code{n.trial} Bernoulli trials for each potential edge with
+#' probabilities given by \code{p} for in-group edges and \code{q} for between
+#' group edges.
 #'
 #' @param n Number of vertices in the graph.
 #' @param p Probability of creating an edge between vertices of the same group.
 #' @param q Probability of creating an edge between vertices of different groups.
 #' @param block.sizes Numeric vector giving the number of vertices in each group.
 #' The sum of the vector must match the number of vertices.
+#' @param n.trials Number of repeated Bernoulli trials per edge. Graphs are
+#' weighted for \code{n.trials} greater than 1.
 #' @param directed Logical scalar, whether to generate a directed graph.
 #' @param loops Logical scalar, whether self-loops are allowed in the graph.
 #' @return An igraph graph.
@@ -18,24 +21,50 @@
 #' @examples
 #'
 #' ## Three groups with only a few connection between groups
-#' g <- sample_ppm(1000, p=0.3, q=0.01, block.sizes=c(100,600,300))
-#' g
+#' g1 <- sample_ppm(1000, p=0.3, q=0.01, block.sizes=c(100,600,300))
+#' g1
+#' ## Three groups with weighted connections.
+#' g2 <- sample_ppm(1000, p=0.1, q=0.003, block.sizes=c(100,600,300), n.trials=3)
+#' g2
 #' @export
-#' @importFrom igraph sample_sbm igraph_opt
+#' @import igraph
 
-sample_ppm <- function (n, p, q, block.sizes, directed = FALSE, loops = FALSE)
+sample_ppm <- function (n, p, q, block.sizes, n.trials = 1,
+                        directed = FALSE, loops = FALSE)
 {
   n <- as.integer(n)
   p <- as.double(p)
   q <- as.double(q)
   block.sizes <- as.integer(block.sizes)
+  n.trials <- as.integer(n.trials)
   directed <- as.logical(directed)
   loops <- as.logical(loops)
   pm <- diag(p, length(block.sizes))
   pm[upper.tri(pm) | lower.tri(pm)] <- q
   res <- sample_sbm(n, pm, block.sizes, directed, loops)
+  if(n.trials > 1){
+    E(res)$weight <- 1
+    for (i in seq(n.trials - 1)){
+      resnew <- sample_sbm(n, pm, block.sizes, directed, loops)
+      E(resnew)$weight <- 1
+      res <- union(res, resnew)
+      E(res)$weight_1[is.na(E(res)$weight_1)] <- 0
+      E(res)$weight_2[is.na(E(res)$weight_2)] <- 0
+      E(res)$weight <- E(res)$weight_1 + E(res)$weight_2
+      res <- remove.edge.attribute(res, "weight_1")
+      res <- remove.edge.attribute(res, "weight_2")
+      # Todo: What about igraph_opt("add.params")?
+      res$name <- res$name_1
+      res <- remove.graph.attribute(res, "name_1")
+      res <- remove.graph.attribute(res, "name_2")
+      res$loops <- res$loops_1
+      res <- remove.graph.attribute(res, "loops_1")
+      res <- remove.graph.attribute(res, "loops_2")
+    }
+  }
   if (igraph_opt("add.params")) {
     res$name <- "Planted partition model"
+    if(n.trials > 1) res$name <- "Weighted planted partition model"
     res$loops <- loops
   }
   res
--- tests/testthat/test-sample.R
+++ tests/testthat/test-sample.R
@@ -21,3 +21,32 @@ test_that("Generating parted partition models works", {
   g4x <- make_full_graph(10, directed=TRUE, loops=TRUE)
   expect_equal(g4[sparse=FALSE], g4x[sparse=FALSE])
 })
+
+
+test_that("Generating weighted parted partition models works", {
+
+  bs <- c(4,6)
+  g1 <- sample_ppm(10, p=1, q=1, block.sizes=bs,
+                   directed=FALSE, loops=FALSE, n.trials = 3)
+  g1x <- make_full_graph(10, directed=FALSE, loops=FALSE)
+  E(g1x)$weight <- 3
+  expect_equal(g1[sparse=FALSE], g1x[sparse=FALSE])
+
+  g2 <- sample_ppm(10, p=1, q=1, block.sizes=bs,
+                   directed=FALSE, loops=TRUE, n.trials = 3)
+  g2x <- make_full_graph(10, directed=FALSE, loops=TRUE)
+  E(g2x)$weight <- 3
+  expect_equal(g2[sparse=FALSE], g2x[sparse=FALSE])
+
+  g3 <- sample_ppm(10, p=1, q=1, block.sizes=bs,
+                   directed=TRUE, loops=FALSE, n.trials = 3)
+  g3x <- make_full_graph(10, directed=TRUE, loops=FALSE)
+  E(g3x)$weight <- 3
+  expect_equal(g3[sparse=FALSE], g3x[sparse=FALSE])
+
+  g4 <- sample_ppm(10, p=1, q=1, block.sizes=bs,
+                   directed=TRUE, loops=TRUE, n.trials = 3)
+  g4x <- make_full_graph(10, directed=TRUE, loops=TRUE)
+  E(g4x)$weight <- 3
+  expect_equal(g4[sparse=FALSE], g4x[sparse=FALSE])
+})
