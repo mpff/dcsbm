cf6720f95b52e0d56116e477b12a2987bf70908d
Manuel Pfeuffer <manuel.pfeuffer@esmt.org>
2022-06-15 09:57:42 +0200

    Add package documentation

---
+++ R/dcsbm.R
@@ -0,0 +1,14 @@
+#' dcsbm: Estimate degree corrected stochastic block models
+#'
+#' The 'dcsbm' packages provides methods for estimating a two-way degree
+#' corrected stochastic block model for directed, weighted graphs. Depends on
+#' the 'igraph' library <https://igraph.org> for graph handling. See Peixoto
+#' (2014) <10.1103/PhysRevE.89.012804> for details on the inference algorithm.
+#'
+#' Estimate a degree corrected stochastic block model: \code{\link{dcsbm}}
+#'
+#' Sample a (weighted) graph from the planted partition model: \code{\link{sample_ppm}}
+#'
+#' @docType package
+#' @name dcsbm
+NULL
